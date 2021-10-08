91e810b7114975e9c0399896d29c4214494dda86
Ricardo Oliveira <ricardo@hashicorp.com>
2021-10-08 12:38:54 +0100

    Update 02_setup_boundary.sh

--- 02_setup_boundary.sh
+++ 02_setup_boundary.sh
@@ -52,20 +52,3 @@ fi
 TARGET_ID=$(cat boundary_target.json | jq -r .item.id)
 
 boundary targets add-host-sets -id=$TARGET_ID -host-set=$HOST_SET_ID
-
-# --- demo ---
-# kubectl config use-context boundary-cluster-1
-
-# export BOUNDARY_ADDR=http://127.0.0.1:9200/
-# export PROJECT_ID=$(boundary scopes list -recursive -format json | jq  -r '.items[] | select(.name | contains("Generated project scope")) | .id')
-
-# AUTH_METHOD_ID=$(cat boundary_login.json | jq -r .auth_method.auth_method_id) &&
-# PASSWORD=$(cat boundary_login.json | jq -r .auth_method.password) && \
-# boundary authenticate password \
-#          -login-name=admin \
-#          -password $PASSWORD \
-#          -auth-method-id=$AUTH_METHOD_ID
-
-# unset BOUNDARY_RECOVERY_CONFIG
-
-# boundary connect kube -target-name=k8s-api  -target-scope-id $PROJECT_ID -- get pods
